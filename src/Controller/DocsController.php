3cf4d2473a80ba2b27b9968c6e7adfb574550911
xachman <zironside@hotmail.com>
2018-02-18 20:14:40 +0000

    create Docs functionality

--- src/Controller/DocsController.php
+++ src/Controller/DocsController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
-
+use Cake\Event\Event;
 /**
  * DoNotCalls Controller
  *
@@ -11,13 +11,28 @@ use App\Controller\AppController;
  */
 class DocsController extends AppController
 {
-
+    private $view_path; 
     public function initialize() {
+        $this->view_path = dirname(dirname(__FILE__))."/Template/Docs";
         parent::initialize();
 
         $this->loadComponent('Markdown.Markdown');
     }
-    public function index() {
 
+    public function beforeFilter(Event $event)
+    {
+        parent::beforeFilter($event);
+
+        
+        $path = $this->view_path."/".join($this->request->params['pass'], "/").'.md';
+        if(!$this->request->params['pass']) {
+            $path = $this->view_path."/index.md";
+        }
+        $md = file_get_contents($path, true);
+        $content = $this->Markdown->parse($md);
+        $this->set(compact('content')); 
+
+    }
+    public function index() {
     }
 }
