7feb9ddcb9e779835013f124ab9394fdd18de7d7
xachman <zironside@hotmail.com>
2018-02-18 20:45:35 +0000

    add nav to docs

--- src/Controller/DocsController.php
+++ src/Controller/DocsController.php
@@ -30,9 +30,34 @@ class DocsController extends AppController
         }
         $md = file_get_contents($path, true);
         $content = $this->Markdown->parse($md);
-        $this->set(compact('content')); 
+        $nav = $this->generateNav();
+
+        $this->set(compact('content', 'nav')); 
 
     }
     public function index() {
     }
+
+    private function generateNav(): string {
+
+        $html = '<ul class="side-nav">';
+        if ($handle = opendir($this->view_path)) {
+            while (false !== ($file = readdir($handle)))
+            {
+                if ($file != "." && $file != ".." && strtolower(substr($file, strrpos($file, '.') + 1)) == 'md')
+                {
+                    $sub = str_replace(substr($file, strrpos($file, '.')), "", $file);
+                    $linkName = ($sub == 'index')?  '' : '/'.$sub;
+                    $name = ($sub == 'index')? 'Home' : ucwords(str_replace('_', ' ', $sub));
+
+                    $html .= '<li><a href="/Docs'.$linkName.'" >'.$name.'</a></li>';
+
+                }
+            }
+            closedir($handle);
+        }
+
+        return $html."</ul>";
+    }
+       
 }
